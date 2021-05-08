24c4f12b89e32887888973d2d0dfa0d144939215
Thomas Elder <MasonCosLive@Mason-Cos-Macbook.local>
2021-05-08 16:09:31 -0600

    removed some connection code

--- server.js
+++ server.js
@@ -18,8 +18,6 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
 });
 
 // routes
